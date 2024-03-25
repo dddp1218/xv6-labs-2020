94a67a1fe7686b65c0b999acf43b68d66e8e31ac
dddp <1328263166@qq.com>
2024-03-25 09:58:57 +0800

    完成了第一个作业

--- user/sleep.c
+++ user/sleep.c
@@ -9,8 +9,7 @@
 // int main(int argc,char* argv[])
 // argc 是命令行总的参数个数  
 // argv[] 是 argc 个参数，其中第 0 个参数是程序的全名，以后的参数是命令行后面跟的用户输入的参数
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     // 如果命令行参数不等于2个，则打印错误信息
     if (argc != 2)
